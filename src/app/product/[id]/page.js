9f99640b8c533ae3b38714ae8580c9680c59a0bb
Tushar <tusharimran470@gmail.com>
2024-06-25 11:30:50 +0600

    dynamic route added

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,10 +1,11 @@
 
 
-const DynamicRoute = ({params}) => {
+const DynamicRoute = ({params, searchParams}) => {
      console.log(params)
     return (
         <div>
              <h1>This is dynamic route : {params?.id}</h1>
+             <h1>search by : {searchParams?.category} , {searchParams?.price}</h1>
         </div>
     );
 };
