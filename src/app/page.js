8246a5a35febea0d479702161ce260480f551315
Tushar <tusharimran470@gmail.com>
2024-06-26 13:24:32 +0600

    use image component using import alias

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Counter from "@/Components/Counter";
+import Image from "next/image";
 import Link from "next/link";
+import nft from '@/assest/NFP.webp'
 
 
 
@@ -17,6 +19,10 @@ const HomePage = () => {
     <Link href='/about'>  <button className="btn btn-secomdary">About</button></Link>
     <Link href='/checkout'>  <button className="btn btn-secomdary">checkout</button></Link>
     <Link href='/contact'>  <button className="btn btn-secomdary">contact</button></Link>
+
+    <div className="p-10 ">
+           <Image src={nft} alt="nft" width={500} height={500}/>
+    </div>
 
     </div>
   );
